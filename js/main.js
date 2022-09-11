9227a25e1715b5805c7e1405f71ce9cbc4fea940
Ivan-nvkz <66069539+Ivan-nvkz@users.noreply.github.com>
2022-09-11 22:55:53 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,480 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+   // Popup start ====================================================================================
+
+   // Получение хеша в адресе сайта
+   function getHash() {
+      if (location.hash) { return location.hash.replace('#', ''); }
+   }
+   // Указание хеша в адресе сайта
+   function setHash(hash) {
+      history.pushState('', '', hash);
+   }
+
+   //==== Вспомогательные модули блокировки прокрутки и скочка =====
+   let bodyLockStatus = true;
+   let bodyLockToggle = (delay = 500) => {
+      if (document.documentElement.classList.contains('lock')) {
+         bodyUnlock(delay);
+      } else {
+         bodyLock(delay);
+      }
+   }
+   let bodyUnlock = (delay = 500) => {
+      let body = document.querySelector("body");
+      if (bodyLockStatus) {
+         let lock_padding = document.querySelectorAll("._lp");
+         setTimeout(() => {
+            for (let index = 0; index < lock_padding.length; index++) {
+               const el = lock_padding[index];
+               el.style.paddingRight = '0px';
+            }
+            body.style.paddingRight = '0px';
+            document.documentElement.classList.remove("lock");
+         }, delay);
+         bodyLockStatus = false;
+         setTimeout(function () {
+            bodyLockStatus = true;
+         }, delay);
+      }
+   }
+   let bodyLock = (delay = 500) => {
+      let body = document.querySelector("body");
+      if (bodyLockStatus) {
+         let lock_padding = document.querySelectorAll("._lp");
+         for (let index = 0; index < lock_padding.length; index++) {
+            const el = lock_padding[index];
+            el.style.paddingRight = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+         }
+         body.style.paddingRight = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+         document.documentElement.classList.add("lock");
+
+         bodyLockStatus = false;
+         setTimeout(function () {
+            bodyLockStatus = true;
+         }, delay);
+      }
+   }
+
+   //==== Вспомогательные модули блокировки прокрутки и скочка   =====
+
+   let popupItem;
+   function initPopups() {
+      popupItem = new HystModal({
+         linkAttributeName: "data-popup",
+         beforeOpen: function (popupItem) {
+            const hash = popupItem.openedWindow.id;
+            setHash(`#${hash}`);
+         },
+         afterClose: function (popupItem) {
+            setHash(window.location.href.split('#')[0]);
+         },
+         // прочие настройки (не обязательно), см. API
+      });
+      // Открытие по хешу
+      if (getHash() && document.querySelector(`#${getHash()}`)) {
+         popupItem.open(`#${getHash()}`);
+      }
+   }
+   initPopups();
+
+   // Popup end =============================================================================================
+
+   //DYNAMIC ADAPT  start ===================================================================================
+
+   function DynamicAdapt(type) {
+      this.type = type;
+   }
+
+   DynamicAdapt.prototype.init = function () {
+      const _this = this;
+      // массив объектов
+      this.оbjects = [];
+      this.daClassname = "_dynamic_adapt_";
+      // массив DOM-элементов
+      this.nodes = document.querySelectorAll("[data-da]");
+
+      // наполнение оbjects объктами
+      for (let i = 0; i < this.nodes.length; i++) {
+         const node = this.nodes[i];
+         const data = node.dataset.da.trim();
+         const dataArray = data.split(",");
+         const оbject = {};
+         оbject.element = node;
+         оbject.parent = node.parentNode;
+         оbject.destination = document.querySelector(dataArray[0].trim());
+         оbject.breakpoint = dataArray[1] ? dataArray[1].trim() : "767";
+         оbject.place = dataArray[2] ? dataArray[2].trim() : "last";
+         оbject.index = this.indexInParent(оbject.parent, оbject.element);
+         this.оbjects.push(оbject);
+      }
+
+      this.arraySort(this.оbjects);
+
+      // массив уникальных медиа-запросов
+      this.mediaQueries = Array.prototype.map.call(this.оbjects, function (item) {
+         return '(' + this.type + "-width: " + item.breakpoint + "px)," + item.breakpoint;
+      }, this);
+      this.mediaQueries = Array.prototype.filter.call(this.mediaQueries, function (item, index, self) {
+         return Array.prototype.indexOf.call(self, item) === index;
+      });
+
+      // навешивание слушателя на медиа-запрос
+      // и вызов обработчика при первом запуске
+      for (let i = 0; i < this.mediaQueries.length; i++) {
+         const media = this.mediaQueries[i];
+         const mediaSplit = String.prototype.split.call(media, ',');
+         const matchMedia = window.matchMedia(mediaSplit[0]);
+         const mediaBreakpoint = mediaSplit[1];
+
+         // массив объектов с подходящим брейкпоинтом
+         const оbjectsFilter = Array.prototype.filter.call(this.оbjects, function (item) {
+            return item.breakpoint === mediaBreakpoint;
+         });
+         matchMedia.addListener(function () {
+            _this.mediaHandler(matchMedia, оbjectsFilter);
+         });
+         this.mediaHandler(matchMedia, оbjectsFilter);
+      }
+   };
+
+   DynamicAdapt.prototype.mediaHandler = function (matchMedia, оbjects) {
+      if (matchMedia.matches) {
+         for (let i = 0; i < оbjects.length; i++) {
+            const оbject = оbjects[i];
+            оbject.index = this.indexInParent(оbject.parent, оbject.element);
+            this.moveTo(оbject.place, оbject.element, оbject.destination);
+         }
+      } else {
+         for (let i = 0; i < оbjects.length; i++) {
+            const оbject = оbjects[i];
+            if (оbject.element.classList.contains(this.daClassname)) {
+               this.moveBack(оbject.parent, оbject.element, оbject.index);
+            }
+         }
+      }
+   };
+
+   // Функция перемещения
+   DynamicAdapt.prototype.moveTo = function (place, element, destination) {
+      element.classList.add(this.daClassname);
+      if (place === 'last' || place >= destination.children.length) {
+         destination.insertAdjacentElement('beforeend', element);
+         return;
+      }
+      if (place === 'first') {
+         destination.insertAdjacentElement('afterbegin', element);
+         return;
+      }
+      destination.children[place].insertAdjacentElement('beforebegin', element);
+   }
+
+   // Функция возврата
+   DynamicAdapt.prototype.moveBack = function (parent, element, index) {
+      element.classList.remove(this.daClassname);
+      if (parent.children[index] !== undefined) {
+         parent.children[index].insertAdjacentElement('beforebegin', element);
+      } else {
+         parent.insertAdjacentElement('beforeend', element);
+      }
+   }
+
+   // Функция получения индекса внутри родителя
+   DynamicAdapt.prototype.indexInParent = function (parent, element) {
+      const array = Array.prototype.slice.call(parent.children);
+      return Array.prototype.indexOf.call(array, element);
+   };
+
+   // Функция сортировки массива по breakpoint и place 
+   // по возрастанию для this.type = min
+   // по убыванию для this.type = max
+   DynamicAdapt.prototype.arraySort = function (arr) {
+      if (this.type === "min") {
+         Array.prototype.sort.call(arr, function (a, b) {
+            if (a.breakpoint === b.breakpoint) {
+               if (a.place === b.place) {
+                  return 0;
+               }
+
+               if (a.place === "first" || b.place === "last") {
+                  return -1;
+               }
+
+               if (a.place === "last" || b.place === "first") {
+                  return 1;
+               }
+
+               return a.place - b.place;
+            }
+
+            return a.breakpoint - b.breakpoint;
+         });
+      } else {
+         Array.prototype.sort.call(arr, function (a, b) {
+            if (a.breakpoint === b.breakpoint) {
+               if (a.place === b.place) {
+                  return 0;
+               }
+
+               if (a.place === "first" || b.place === "last") {
+                  return 1;
+               }
+
+               if (a.place === "last" || b.place === "first") {
+                  return -1;
+               }
+
+               return b.place - a.place;
+            }
+
+            return b.breakpoint - a.breakpoint;
+         });
+         return;
+      }
+   };
+
+   const da = new DynamicAdapt("max");
+   da.init();
+
+
+   // DYNAMIC ADAPT  end =====================================================================================
+
+
+   // Slider ===============================================================================================
+
+   // const swiper = new Swiper('.popular-slider', {
+   //    // Optional parameters
+   //    // direction: 'vertical',
+   //    loop: true,
+   //    slidesPerView: 3,
+   //    spaceBetween: 30,
+   //    // Автопрокрутка
+   //    // autoplay: {
+   //    // Пауза между прокруткой
+   //    // delay: 3000,
+   //    // Закончить на последнем слайде
+   //    // stopOnLastSlide: true,
+   //    // Отключить после ручного переключения
+   //    // disableOnInteraction: true,
+   //    // },
+
+   //    // Скорость
+   //    speed: 800,
+
+   //    // If we need pagination
+   //    pagination: {
+   //       el: '.swiper-pagination',
+   //    },
+
+   //    // Navigation arrows
+   //    navigation: {
+   //       nextEl: '.swiper-button-next',
+   //       prevEl: '.swiper-button-prev',
+   //    },
+
+   //    // And if we need scrollbar
+   //    scrollbar: {
+   //       el: '.swiper-scrollbar',
+   //    },
+
+   //    breakpoints: {
+   //       320: {
+   //          // slidesPerView: 1,
+   //          // spaceBetween: -20,
+
+   //       },
+   //       425: {
+   //          slidesPerView: 1,
+   //          spaceBetween: 30,
+   //       },
+   //       768: {
+   //          slidesPerView: 2,
+   //          spaceBetween: 30,
+   //       },
+   //       992: {
+   //          slidesPerView: 3,
+   //          spaceBetween: 30,
+   //       },
+   //    },
+
+   // });
+
+   // Slider ===============================================================================================
+   // Slider nmnmmmmmmm ===============================================================================================
+
+
+   let popularSlider = null;
+   let mediaQuerySize = 425;
+
+   function popularSliderInit() {
+      if (!popularSlider) {
+         popularSlider = new Swiper('.popular-slider', {
+            // ... ваши опции
+            loop: true,
+            slidesPerView: 3,
+            spaceBetween: 30,
+            speed: 800,
+            // Navigation arrows
+            navigation: {
+               nextEl: '.swiper-button-next',
+               prevEl: '.swiper-button-prev',
+            },
+
+            breakpoints: {
+               320: {
+                  // slidesPerView: 1,
+                  // spaceBetween: -20,
+
+               },
+               425: {
+                  slidesPerView: 1,
+                  spaceBetween: 30,
+               },
+               768: {
+                  slidesPerView: 2,
+                  spaceBetween: 30,
+               },
+               992: {
+                  slidesPerView: 3,
+                  spaceBetween: 30,
+               },
+            },
+         });
+      }
+   }
+
+   function popularSliderDestroy() {
+      if (popularSlider) {
+         popularSlider.destroy();
+         popularSlider = null;
+      }
+   }
+
+   $(window).on('load resize', function () {
+      // Берём текущую ширину экрана
+      var windowWidth = $(this).innerWidth();
+
+      // Если ширина экрана меньше или равна mediaQuerySize(1024)
+      if (windowWidth > mediaQuerySize) {
+         // Инициализировать слайдер если он ещё не был инициализирован
+         popularSliderInit()
+      } else {
+         // Уничтожить слайдер если он был инициализирован
+         popularSliderDestroy()
+      }
+   });
+
+
+   // Slider nmnmmmmmmm ===============================================================================================
+
+
+
+
+   // Плавный скролл кнопки наверх  ====================================================
+   let btnUp = document.querySelector('.btn__up');
+
+   btnUp.addEventListener('click', function (e) {
+      scrollToY(0);
+   });
+
+   let scrolls = 0;
+   window.addEventListener('scroll', function (e) {
+      // console.log(++scrolls);
+      let pos = window.pageYOffset;
+
+      if (pos > window.innerHeight) {
+         btnUp.classList.add('btn__up-open');
+      }
+      else {
+         btnUp.classList.remove('btn__up-open');
+      }
+
+   });
+
+   function scrollToY(pos) {
+      window.scrollTo({
+         top: pos,
+         behavior: "smooth"
+      });
+   }
+   // Плавный скролл кнопки наверх  =================================================================
+
+
+   // Плавный скролл к пунктам =======================================================================
+   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+         e.preventDefault();
+         document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+         });
+      });
+   });
+   // Плавный скролл к пунктам =========================================================================
+
+
+
+   // Меню бургер ======================================================================================
+   //Burger start   ====================================================================================
+   const iconMenu = document.querySelector(".icon-menu");
+   const menuBody = document.querySelector(".menu__list");
+
+   if (iconMenu) {
+      iconMenu.addEventListener("click", function (e) {
+         document.body.classList.toggle("_lock");
+         iconMenu.classList.toggle("menu-open");
+         menuBody.classList.toggle("menu-open");
+         menuBody.classList.toggle('menu__list--active');
+      });
+   }
+   if (menuBody) {
+      menuBody.addEventListener('click', function () {
+         iconMenu.classList.remove("menu-open");
+         menuBody.classList.remove("menu__list--active");
+         menuBody.classList.remove("menu-open");
+         document.body.classList.remove("_lock");
+      });
+   }
+
+   //Burger  end  ==========================================================================================
+
+
+
+
+});
+
+//=====  JQuery  start  =============================================================
+
+$(document).ready(function () {
+   $("form").submit(function () { // Событие отправки с формы
+      var form_data = $(this).serialize(); // Собираем данные из полей
+      $.ajax({
+         type: "POST", // Метод отправки
+         url: "send.php", // Путь к PHP обработчику sendform.php
+         data: form_data,
+         success: function () {
+            $('.overley').addClass('overley-visible');
+            $('.modal').addClass('modal__visible');
+         }
+      }).done(function () {
+         $(this).find('input').val('');
+         $('form').trigger('reset');
+      });
+      event.preventDefault();
+   });
+});
+
+
+// Slick slider start ====================================================================
+$(function () {
+   $('.your-class').slick({
+      dots: true,
+   });
+
+});
+
+// Slick slider finish ====================================================================
+
+//=====  JQuery  finish ===================================================================
